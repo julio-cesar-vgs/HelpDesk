f46ee09aa626345b486f5a1c688a7b76665918b1
Sleepk <julio.cesar.vgs@gmail.com>
2018-11-17 12:03:15 -0200

    configurando o ticket

---
+++ helpdesk/src/main/java/com/schoolofnet/helpdesk/models/Ticket.java
@@ -0,0 +1,104 @@
+package com.schoolofnet.helpdesk.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tickets")
+public class Ticket {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+
+	@Column(name = "descricao")
+	private String desc;
+
+	private Date created;
+
+	private Date closed;
+
+	private Boolean finished;
+
+	private User userOpen;
+
+	private User techinician;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+	public Date getClosed() {
+		return closed;
+	}
+
+	public void setClosed(Date closed) {
+		this.closed = closed;
+	}
+
+	public Boolean getFinished() {
+		return finished;
+	}
+
+	public void setFinished(Boolean finished) {
+		this.finished = finished;
+	}
+
+	public User getUserOpen() {
+		return userOpen;
+	}
+
+	public void setUserOpen(User userOpen) {
+		this.userOpen = userOpen;
+	}
+
+	public User getTechinician() {
+		return techinician;
+	}
+
+	public void setTechinician(User techinician) {
+		this.techinician = techinician;
+	}
+
+	public Ticket() {
+	}
+	
+	
+
+}
