7b6b3e88130ec143422d47f177ed53d7f72989c8
Sleepk <julio.cesar.vgs@gmail.com>
2018-11-17 11:06:33 -0200

    Configurando a tela inicial

--- helpdesk/src/main/java/com/schoolofnet/helpdesk/controller/HomeController.java
+++ helpdesk/src/main/java/com/schoolofnet/helpdesk/controller/HomeController.java
@@ -3,12 +3,16 @@ package com.schoolofnet.helpdesk.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class AuthController {
+@RequestMapping("/")
+public class HomeController {
 
-	@GetMapping("/login")
-	public String login(Model model) {
-		return "auth/login";
+	
+	@GetMapping
+	public String index(Model model) {
+		return "home/index";
 	}
+
 }
